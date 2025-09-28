d9ac7e0a7f03f745e0e11c09233271e912dc757c
Aletu <alesolorzano00@gmail.com>
2025-09-28 13:17:01 -0600

    Add Tailwind CSS and DaisyUI configuration files
    Introduces postcss.config.cjs and tailwind.config.cjs to enable Tailwind CSS with DaisyUI plugin. Configures content paths and sets up basic themes for styling.

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{svelte,js,ts}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light","cupcake"]
+  }
+};
